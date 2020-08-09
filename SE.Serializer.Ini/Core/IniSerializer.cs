00c16fe6f9e79b6d891ede531fe73878a9a0b769
GeoSTGames <55875553+GeoSTGames@users.noreply.github.com>
2020-08-10 01:47:02 +1000

    Started work on a custom ini parser and serializer. Mostly planning.

--- SE.Serializer.Ini/Core/IniSerializer.cs
+++ SE.Serializer.Ini/Core/IniSerializer.cs
@@ -6,6 +6,34 @@ namespace SE.Core
 {
     public static class IniSerializer
     {
+        // How the ini serializer will work:
+        //     - Entries are grouped into sections. Entries are pretty much an entry within a Dictionary<string, string>.
+        //
+        // THE PARSER:
+        //     - Set current section to default / null.
+        //     - Loop through a string separated by newline. For each line:
+        //         - IF:      Starts with comment char '#', then add the line to a list called NextElementComments.
+        //         - ELSE IF: Starts with the section start char '[', then set the current section to whatever is within the brackets.
+        //         - ELSE:    Create an entry for the current section, where:
+        //                    KEY   = Left side of the separator char '='.
+        //                    VALUE = Right side of the separator char.
+        // 
+        // THE SERIALIZER:
+        //     - The serializer is used to serialize custom classes from and to INI.
+        //     - For reading, works after the parser has done it's work.
+        //     - Use attributes to separate sections within custom classes.
+        //     TODO: Plan.
 
+        // PARSER ATTRIBUTES EXAMPLE:
+        //
+        // public class Example
+        // {
+        //     [IniSection(Name: "Screen", Comments: "Screen settings.")]
+        //     public int ScreenSizeX = 1920;
+        //     public int ScreenSizeY = 1080;
+        //
+        //     [IniSection(Name: "User", Comments: "User settings.")]
+        //     public string Name = "Bob";
+        // }
     }
 }
---
+++ SE.Serializer.Ini/Engine/Serialization/Ini/Parser/ParserSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace SE.Serialization.Ini.Parser
+{
+    public class ParserSettings
+    {
+        public ParserCharacters Characters = new ParserCharacters();
+
+        public static ParserSettings Default { get; } = new ParserSettings();
+
+        public class ParserCharacters
+        {
+            public char NewLine = '\n';
+            public char Separator = '=';
+            public char Comment = '#';
+            public char SectionStart = '[';
+            public char SectionEnd = ']';
+        }
+    }
+}
