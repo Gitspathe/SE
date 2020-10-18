6ae5274e0fa2c0a620d30caa18b32336f2989735
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2020-10-18 20:07:10 +1100

    Moved native particles to it's own mini-library

---
+++ SE.Particles.Native/Engine/Particles/Modules/Native/NativeAlphaModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SE.Particles.Modules.Native
+{
+    public sealed unsafe class NativeAlphaModule : NativeSubmodule
+    {
+        internal NativeAlphaModule(NativeModule module) : base(module)
+        {
+            SubmodulePtr = nativeModule_AlphaModule_Ctor(ModulePtr);
+        }
+
+        public void SetNone() => nativeModule_AlphaModule_SetNone(SubmodulePtr);
+        public void SetLerp(float end) => nativeModule_AlphaModule_SetLerp(SubmodulePtr, end);
+        public void SetRandomLerp(float min, float max) => nativeModule_AlphaModule_SetRandomLerp(SubmodulePtr, min, max);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern Submodule* nativeModule_AlphaModule_Ctor(Module* modulePtr);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_AlphaModule_SetNone(Submodule* modulePtr);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_AlphaModule_SetLerp(Submodule* modulePtr, float end);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_AlphaModule_SetRandomLerp(Submodule* modulePtr, float min, float max);
+    }
+}
---
+++ SE.Particles.Native/Engine/Particles/Modules/Native/NativeModule.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SE.Particles.Modules.Native
+{
+    // TODO.
+    // TODO: Native module builder.
+    public unsafe class NativeModule : ParticleModule, IDisposable
+    {
+        internal Module* NativeModulePtr;
+
+        private bool isDisposed;
+
+        public NativeModule()
+        {
+            NativeModulePtr = (Module*)nativeModule_Create();
+            
+            NativeAlphaModule alpha = new NativeAlphaModule(this);
+            alpha.SetLerp(0.0f);
+        }
+
+        public override void OnParticlesActivated(Span<int> particlesIndex)
+        {
+            fixed (int* indexPtr = particlesIndex) {
+                nativeModule_OnParticlesActivated(NativeModulePtr, indexPtr, Emitter.GetParticlePointer(), particlesIndex.Length);
+            }
+        }
+
+        public override void OnUpdate(float deltaTime, Particle* arrayPtr, int length)
+        {
+            nativeModule_OnUpdate(NativeModulePtr, deltaTime, arrayPtr, length);
+        }
+
+        public override void OnInitialize()
+        {
+            nativeModule_OnInitialize(NativeModulePtr, Emitter.ParticlesLength);
+        }
+
+        public override ParticleModule DeepCopy()
+        {
+            return null;
+        }
+
+        ~NativeModule()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose()
+        {
+            if (isDisposed)
+                return;
+
+            nativeModule_Delete(NativeModulePtr);
+            isDisposed = true;
+        }
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void* nativeModule_Create();
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_AddSubmodule(Module* modulePtr, Submodule* submodulePtr);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_RemoveSubmodule(Module* modulePtr, Submodule* submodulePtr);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_OnInitialize(Module* modulePtr, int particleArrayLength);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_OnParticlesActivated(Module* modulePtr, int* particleIndexArr, Particle* particleArrPtr, int length);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_OnUpdate(Module* modulePtr, float deltaTime, Particle* particleArrPtr, int length);
+
+        [DllImport("SE.Native", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void nativeModule_Delete(Module* modulePtr);
+    }
+
+    internal struct Module { /* Used for naming only! */ }
+    internal struct Submodule { /* Used for naming only! */ }
+
+    public unsafe class NativeSubmodule
+    {
+        internal Module* ModulePtr;
+        internal Submodule* SubmodulePtr;
+
+        internal NativeSubmodule(NativeModule module) => ModulePtr = module.NativeModulePtr;
+    }
+}
